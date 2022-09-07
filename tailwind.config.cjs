b555910cec4a13b7cbcd307142681a3ec23e15e2
mariohamann <mario.hamann@virtual-identity.com>
2022-09-07 17:26:27 +0200

    feat: create header

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,18 @@
+const colors = require('tailwindcss/colors')
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Fira Sans', 'sans-serif'],
+        serif: ['Playfair Display', 'serif'],
+      },
+      colors: {
+        gray: colors.stone,
+      }
+    },
 	},
 	plugins: [],
 }
